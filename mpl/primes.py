6a05e71d8b2707809b164ca2df61da119f4fbf2c
andrzejmp <andrzejmp@protonmail.com>
2025-11-16 16:41:25 +0100

    some changes

---
+++ mpl/primes.py
@@ -0,0 +1,19 @@
+def prime(n):
+    is_prime = True
+    if n == 1:
+        return False
+    else:
+        for i in range(2, (int)(n//2)+1):
+            if n % i == 0:
+                is_prime = False
+                break
+        return is_prime        
+
+for i in range(1, 50):
+    if prime(i):
+        print(i, end= " ")
+
+print('\n----------------------')
+
+print(prime(1))
+
